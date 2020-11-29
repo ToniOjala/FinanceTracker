578de939d8a785a09fc607c638703f0bfeaef193
Toni Ojala <toni.ojala05@gmail.com>
2020-11-29 20:19:52 +0200

    Simplify the form adding category

--- frontend/src/features/AddCategoryDialog/AddCategoryForm.tsx
+++ frontend/src/features/AddCategoryDialog/AddCategoryForm.tsx
@@ -1,8 +1,9 @@
-import { Button } from '@material-ui/core';
-import { Field, Form, Formik } from 'formik';
+import { Button, MenuItem, Select, TextField } from '@material-ui/core';
+import { useFormik } from 'formik';
 import React from 'react';
 import { NewCategory, TransactionType } from '../../types';
-import { TextBox, TypeOption, TypeSelection } from './FormFields';
+import { TypeOption } from './FormFields';
+import * as yup from 'yup';
 
 interface Props {
   onSubmit: (values: NewCategory) => void;
@@ -14,43 +15,51 @@ const typeOptions: TypeOption[] = [
   { value: TransactionType.Income, label: 'Income' }
 ];
 
+const validationSchema = yup.object({
+  name: yup
+    .string()
+    .required('Name is required'),
+});
+
 const AddCategoryForm = ({ onSubmit, onCancel }: Props): JSX.Element => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      type: TransactionType.Expense
+    },
+    validationSchema: validationSchema,
+    onSubmit: onSubmit,
+  })
+
   return (
-    <Formik
-      initialValues={{
-        name: "",
-        type: TransactionType.Expense
-      }}
-      onSubmit={onSubmit}
-      validate={values => {
-        const requiredError = 'Field is required';
-        const errors: { [field: string]: string } = {};
-        if (!values.name) errors.name = requiredError;
-        if (!values.type) errors.type = requiredError;
-        return errors;
-      }}
-    >
-      {({ isValid, dirty }) => {
-        return (
-          <Form className="form ui">
-            <Field
-              label="Name"
-              placeholder="Name"
-              name="name"
-              component={TextBox}
-            />
-            <TypeSelection
-              label="Type"
-              name="gender"
-              options={typeOptions}
-            />
-            <Button variant="contained" color="secondary" onClick={onCancel}>Cancel</Button>
-            <Button variant="contained" color="primary" disabled={!dirty || !isValid}>Create</Button>
-          </Form>
-        )
-      }}
-    </Formik>
+    <form onSubmit={formik.handleSubmit}>
+      <TextField
+        fullWidth
+        id="name"
+        name="name"
+        label="Name"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+        error={formik.touched.name && Boolean(formik.errors.name)}
+        helperText={formik.touched.name && formik.errors.name}
+      />
+      <Select
+        fullWidth
+        id="type"
+        name="type"
+        label="Type"
+        value={formik.values.type}
+        onChange={formik.handleChange}
+        error={formik.touched.type && Boolean(formik.errors.type)}
+      >
+        {typeOptions.map(opt => (
+          <MenuItem key={opt.value} value={opt.value}>{opt.label}</MenuItem>
+        ))}
+      </Select>
+      <Button variant="contained" color="secondary" onClick={onCancel}>Cancel</Button>
+      <Button variant="contained" color="primary">Create</Button>
+    </form>
   )
-}
+};
 
 export default AddCategoryForm;
