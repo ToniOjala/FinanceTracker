6d06525494c39f066bf66b48e9514d3f1e6be9a3
Toni Ojala <toni.ojala05@gmail.com>
2020-11-29 09:16:32 +0200

    Add type property to category model

--- backend/models/category.ts
+++ backend/models/category.ts
@@ -1,13 +1,17 @@
 import { model, Schema, Document } from 'mongoose';
+import { TransactionType } from './transaction';
 
 interface ICategory {
-  name: string
+  name: string,
+  type: TransactionType,
 }
 
 interface ICategoryDoc extends ICategory, Document {}
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const CategorySchemaFields: Record<keyof ICategory, any> = {
-  name: { type: String, required: true, unique: true }
+  name: { type: String, required: true, unique: true },
+  type: { type: TransactionType, required: true }
 };
 
 const CategorySchema = new Schema(CategorySchemaFields);
