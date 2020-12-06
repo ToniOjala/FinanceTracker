668c789f685e79298cbe60e822fb60ddc815249b
Toni Ojala <toni.ojala05@gmail.com>
2020-12-06 21:44:03 +0200

    Add service method for get by date and category

--- frontend/src/services/transactionService.ts
+++ frontend/src/services/transactionService.ts
@@ -1,3 +1,4 @@
+import { ParsableDate } from "@material-ui/pickers/constants/prop-types";
 import { Transaction } from "../types";
 import { get } from "./apiService";
 
@@ -5,4 +6,8 @@ const url = 'http://localhost:3001/api/transactions';
 
 export const getTransactionsByCategory = (category: string): Promise<Transaction[]> => {
   return get<Transaction[]>(`${url}/${category}`);
+}
+
+export const getTransactionsByDateAndCategory = (date: ParsableDate, category: string): Promise<Transaction[]> => {
+  return get<Transaction[]>(`${url}/date/${date}/${category}`);
 }
