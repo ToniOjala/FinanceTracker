0b7e3f852e71860bacfaf2c11e607184babfcaec
Toni Ojala <toni.ojala05@gmail.com>
2020-12-07 14:33:31 +0200

    Add SideNav component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core';
 import moment from 'moment';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SideNav from './components/SideNav';
 import MonthView from './features/MonthView';
 import YearMonthSelector from './features/YearMonthSelector';
 import YearView from './features/YearView';
@@ -21,23 +22,39 @@ const theme = createMuiTheme({
   }
 })
 
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+  }
+})
+
 const App = (): JSX.Element | null => {
   const [yearMonth, setYearMonth] = useState<YearMonth>({ year: moment().year(), month: moment().month() + 1 });
 
+  const classes = useStyles();
+
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <YearMonthSelector setYearMonth={setYearMonth} />
-      <Router>
-        <Switch>
-          <Route path="/year">
-            <YearView yearMonth={yearMonth} />
-          </Route>
-          <Route path="/">
-            <MonthView yearMonth={yearMonth} />
-          </Route>
-        </Switch>
-      </Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <SideNav />
+        <main className={classes.content}>
+          <YearMonthSelector setYearMonth={setYearMonth} />
+          <Router>
+            <Switch>
+              <Route path="/year">
+                <YearView yearMonth={yearMonth} />
+              </Route>
+              <Route path="/">
+                <MonthView yearMonth={yearMonth} />
+              </Route>
+            </Switch>
+          </Router>
+        </main>
+      </div>
     </ThemeProvider>
   )
 }
---
+++ frontend/src/components/SideNav.tsx
@@ -0,0 +1,30 @@
+import { Button, Drawer, makeStyles } from '@material-ui/core'
+import React from 'react'
+
+const useStyles = makeStyles({
+  root: {
+    width: 100,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: 100
+  }
+})
+
+const SideNav = () => {
+  const classes = useStyles();
+  
+  return (
+    <Drawer
+      className={classes.root}
+      variant="permanent"
+      classes={{ paper: classes.drawerPaper}}
+      anchor="left"
+    >
+      <Button>Month</Button>
+      <Button>Year</Button>
+    </Drawer>
+  )
+}
+
+export default SideNav
