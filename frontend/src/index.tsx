7b4d16f4b55e050402df672f5aaa5392a6e79263
Toni Ojala <toni.ojala05@gmail.com>
2020-12-08 20:25:35 +0200

    Import store from file instead of creating it in index.ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-
-const hello = () => ('hello');
-const store = configureStore({ reducer: hello });
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
