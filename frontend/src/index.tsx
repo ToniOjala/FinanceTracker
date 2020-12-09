0d737fe25ccb770950f5efbd608bda3dc83f03d1
Toni Ojala <toni.ojala05@gmail.com>
2020-12-09 20:16:13 +0200

    Fix index.tsx according to redux toolkit tutorial

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,11 +5,19 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
-);
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
+  );
+}
+
+render();
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./App', render);
+}
