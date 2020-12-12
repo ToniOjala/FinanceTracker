0cf17513b255533f8e3d8949357ac8344ac68521
Toni Ojala <toni.ojala05@gmail.com>
2020-12-12 21:51:32 +0200

    Add route for getting the sum of transactions of a year by category

--- backend/routes/transactions.ts
+++ backend/routes/transactions.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Category } from '../models/category';
 import { Transaction } from '../models/transaction';
 import { getTransactions } from '../services/transactionService';
 import { toNewTransaction } from '../utils';
@@ -17,12 +18,19 @@ router.get('/', async (request, response) => {
 
 router.get('/sum', async (request, response) => {
   const year = Number(request.query.year);
-  const month = Number(request.query.month);
-  const category = request.query.category?.toString();
-  const transactions = await getTransactions(year, month, category);
-  const sum = transactions.reduce((acc, tran) => acc + tran.amount, 0);
 
-  response.send(sum);
+  if (!year || isNaN(year)) response.status(400);
+
+  const categories = await Category.find({});
+
+  const sumsByCategory: Map<string, number> = new Map<string, number>();
+
+  for (const category of categories) {
+    const transactions = await getTransactions(year, undefined, category.name);
+    sumsByCategory.set(category.name, transactions.reduce((acc, tran) => acc + tran.amount, 0));
+  }
+
+  response.send(JSON.stringify([...sumsByCategory]));
 });
 
 router.post('/', async (request, response) => {
