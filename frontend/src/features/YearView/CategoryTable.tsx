99fff959225de5d3bd6209c1848cc52c04736070
Toni Ojala <toni.ojala05@gmail.com>
2020-12-12 22:37:30 +0200

    Bring YearView to a default state without any data

--- frontend/src/features/YearView/CategoryTable.tsx
+++ frontend/src/features/YearView/CategoryTable.tsx
@@ -1,12 +1,16 @@
-import { Table, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectSumsByCategory } from '../../slices/transactions';
 import { months } from './constants'
 
 interface Props {
-  title: string
+  title: string,
 }
 
 const CategoryTable = ({ title }: Props): JSX.Element => {
+  const sumsByCategory = useSelector(selectSumsByCategory);
+
   return (
     <TableContainer>
       <Table>
@@ -18,6 +22,8 @@ const CategoryTable = ({ title }: Props): JSX.Element => {
             )}
           </TableRow>
         </TableHead>
+        <TableBody>
+        </TableBody>
       </Table>
     </TableContainer>
   )
--- frontend/src/features/YearView/index.tsx
+++ frontend/src/features/YearView/index.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../../rootReducer'
-import { fetchTransactionsOfYear } from '../../slices/transactions'
+import { selectYear } from '../../slices/dateSelection'
+import { fetchSumsByCategory } from '../../slices/transactions'
 import CategoryTable from './CategoryTable'
 
 const YearView = (): JSX.Element => {
   const dispatch = useDispatch();
-  const yearMonth = useSelector((state: RootState) => state.dateSelection.yearMonth);
-  const categories = useSelector((state: RootState) => state.categories);
-  const transactions = useSelector((state: RootState) => state.transactions);
+  const year = useSelector(selectYear);
 
   useEffect(() => {
-    dispatch(fetchTransactionsOfYear(yearMonth.year));
-  }, [yearMonth]);
+    dispatch(fetchSumsByCategory(year));
+  }, [year]);
 
   return (
     <>
