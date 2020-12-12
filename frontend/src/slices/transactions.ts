c20b66bd93944faa3ea848f2e78d5cc3ddcc7552
Toni Ojala <toni.ojala05@gmail.com>
2020-12-12 22:38:25 +0200

    Add sumsByCategory to redux state

--- frontend/src/slices/transactions.ts
+++ frontend/src/slices/transactions.ts
@@ -1,20 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createSlice } from "@reduxjs/toolkit";
 import { AppThunk } from '../store';
-import { getTransactionsByCategory, getTransactionsOfMonth, getTransactionsOfYear, saveTransaction } from "../services/transactionService";
-import { Transaction, YearMonth } from "../types";
+import { getSumsByCategory, getTransactionsByCategory, getTransactionsOfMonth, getTransactionsOfYear, saveTransaction } from "../services/transactionService";
+import { Transaction, YearMonth, SumsByCategory } from "../types";
+import { RootState } from "../rootReducer";
+
+const initialState = {
+  transactions: [] as Transaction[],
+  sumsByCategory: {} as SumsByCategory,
+}
 
 const transactionSlice = createSlice({
   name: 'transaction',
-  initialState: [] as Transaction[],
+  initialState: initialState,
   reducers: {
     setTransactions: (state, action) => {
-      return action.payload;
+      state.transactions = action.payload;
+    },
+    setSumsByCategory: (state, action) => {
+      state.sumsByCategory = action.payload;
     },
     addTransaction: (state, action) => {
       saveTransaction(action.payload)
         .then(transaction => {
-          state.push(transaction);
+          state.transactions.push(transaction);
         })
         .catch(error => {
           console.log(error.message);
@@ -23,7 +32,7 @@ const transactionSlice = createSlice({
   }
 })
 
-export const { setTransactions, addTransaction } = transactionSlice.actions;
+export const { setTransactions, setSumsByCategory, addTransaction } = transactionSlice.actions;
 export default transactionSlice.reducer;
 
 export const fetchTransactionsByCategory = (category: string): AppThunk => async dispatch => {
@@ -31,7 +40,7 @@ export const fetchTransactionsByCategory = (category: string): AppThunk => async
     const transactions = await getTransactionsByCategory(category);
     dispatch(setTransactions(transactions));
   } catch (error) {
-    console.log('Error while fetcing transactions: ', error);
+    console.error('Error while fetcing transactions: ', error);
   }
 }
 
@@ -40,7 +49,7 @@ export const fetchTransactionsOfMonth = (yearMonth: YearMonth): AppThunk => asyn
     const transactions = await getTransactionsOfMonth(yearMonth);
     dispatch(setTransactions(transactions));
   } catch (error) {
-    console.log('Error while fetching transactions: ', error);
+    console.error('Error while fetching transactions: ', error);
   }
 }
 
@@ -49,6 +58,18 @@ export const fetchTransactionsOfYear = (year: number): AppThunk => async dispatc
     const transactions = await getTransactionsOfYear(year);
     dispatch(setTransactions(transactions));
   } catch (error) {
-    console.log('Error while fetching transactions: ', error);
+    console.error('Error while fetching transactions: ', error);
   }
 }
+
+export const fetchSumsByCategory = (year: number): AppThunk => async dispatch => {
+  try {
+    const sumsByCategory = await getSumsByCategory(year);
+    dispatch(setSumsByCategory(sumsByCategory));
+  } catch (error) {
+    console.error('Error while fetching sums by categories: ', error);
+  }
+}
+
+export const selectTransactions = (state: RootState): Transaction[] => state.transactions.transactions;
+export const selectSumsByCategory = (state: RootState): SumsByCategory => state.transactions.sumsByCategory;
