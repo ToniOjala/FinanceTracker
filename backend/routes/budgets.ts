fb828fb7586e6543c4f2f4f9af7a0931e6519488
Toni Ojala <toni.ojala05@gmail.com>
2020-12-16 22:40:37 +0200

    Add /latest route for budgets

--- backend/routes/budgets.ts
+++ backend/routes/budgets.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { Budget, IBudget } from '../models/budget';
+import { Category, ICategory } from '../models/category';
 import { budgetExists, toNewBudgets } from '../utils';
 
 const router = express.Router();
@@ -7,6 +8,34 @@ const router = express.Router();
 router.get('/', async (_request, response) => {
   const budgets = await Budget.find({});
   response.json(budgets);
+});
+
+router.get('/latest', async (request, response) => {
+  const year = Number(request.query.year);
+  const month = Number(request.query.month);
+
+  if (!year || !month ) response.status(400);
+  if (isNaN(year) || isNaN(month)) {
+    response.status(400);
+    response.send(`Year: ${year} or month: ${month} not set properly.`);
+    return;
+  }
+
+  const date = new Date(year, month - 1, 1, 12);
+
+  const categories: ICategory[] = await Category.find({});
+  const latestBudgetPerCategory: { [key: string]: number } = {};
+
+  for (const category of categories) {
+    try {
+      const budgets: IBudget[] = await Budget.find({ category: category.name, startDate: { $lte: date} });
+      latestBudgetPerCategory[category.name] = budgets[0].amount;
+    } catch (error) {
+      latestBudgetPerCategory[category.name] = 0;
+    }
+  }
+
+  response.json(latestBudgetPerCategory);
 });
 
 router.post('/', async (request, response) => {
@@ -20,7 +49,7 @@ router.post('/', async (request, response) => {
       createdBudgets.push(createdBudget);
     }
   }
-  
+
   response.json(createdBudgets);
 });
 
