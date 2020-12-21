fd2e106f620c84ecd1341a9bad6535e533aecd99
Toni Ojala <toni.ojala05@gmail.com>
2020-12-21 23:34:00 +0200

    Add Ipc channel Database

---
+++ frontend/src/electron/IPC/DatabaseChannel.ts
@@ -0,0 +1,25 @@
+import { IpcChannel, IpcRequest } from './IpcChannel';
+import { IpcMainEvent } from 'electron';
+import sqliteDB, { Database } from 'better-sqlite3';
+
+export class DatabaseChannel implements IpcChannel {
+  private db: Database;
+  
+  constructor() {
+    this.db = new sqliteDB('../../../../db.db', { verbose: console.log });
+  }
+
+  getName(): string { return 'database'; }
+
+  handle(event: IpcMainEvent, request: IpcRequest): void {
+    if (!request.responseChannel) request.responseChannel = `${this.getName()}_response`;
+    
+    let sql = '';
+    if (request.params) {
+      sql = request.params[0];
+      const statement = this.db.prepare(sql);
+      const result = statement.run();
+      event.sender.send(request.responseChannel, { result: result });
+    }
+  }
+}
