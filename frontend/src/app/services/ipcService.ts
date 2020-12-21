bb5d72e5b9be66f8ec757b183894ed3ab4c7289f
Toni Ojala <toni.ojala05@gmail.com>
2020-12-21 23:49:55 +0200

    Add ipcService

---
+++ frontend/src/app/services/ipcService.ts
@@ -0,0 +1,25 @@
+import { IpcRenderer } from 'electron';
+import { IpcRequest } from '../../shared/IpcRequest';
+
+export class IpcService {
+  private ipcRenderer?: IpcRenderer;
+
+  public send<T>(channel: string, request: IpcRequest = {}): Promise<T> {
+    if (!this.ipcRenderer) this.initializeIpcRenderer();
+    if (!request.responseChannel) request.responseChannel = `${channel}_response_${new Date().getTime()}`;
+
+    this.ipcRenderer?.send(channel, request);
+
+    return new Promise(resolve => {
+      this.ipcRenderer?.once(request.responseChannel || '', (event, response) => resolve(response));
+    });
+  }
+
+  private initializeIpcRenderer() {
+    if (!window || !window.process || !window.require) {
+      throw new Error('Unable to require renderer process');
+    }
+
+    this.ipcRenderer = window.require('electron').ipcRenderer;
+  }
+}
