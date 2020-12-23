f3202f708f70559b020a5ade148723719bd69aa2
Toni Ojala <toni.ojala05@gmail.com>
2020-12-23 23:06:49 +0200

    Move database init inside handle method

--- frontend/src/electron/IPC/DatabaseChannel.ts
+++ frontend/src/electron/IPC/DatabaseChannel.ts
@@ -1,26 +1,29 @@
 import { IpcChannel } from './IpcChannel';
 import { IpcMainEvent } from 'electron';
-import sqliteDB, { Database } from 'better-sqlite3';
+import sqliteDB from 'better-sqlite3';
 import { IpcRequest } from '../../shared/IpcRequest';
 
 export class DatabaseChannel implements IpcChannel {
-  private db: Database;
-  
-  constructor() {
-    this.db = new sqliteDB('../../../../db.db', { verbose: console.log });
-  }
-
   getName(): string { return 'database'; }
 
   handle(event: IpcMainEvent, request: IpcRequest): void {
     if (!request.responseChannel) request.responseChannel = `${this.getName()}_response`;
-    
+
+    const db = new sqliteDB('C:/Users/Toni/Repos/Helsinki University/FinanceTracker/db.db', { verbose: console.log });
+
     let sql = '';
     if (request.params) {
-      sql = request.params[0];
-      const statement = this.db.prepare(sql);
-      const result = statement.run();
-      event.sender.send(request.responseChannel, { result: result });
+      try {
+        sql = request.params[0];
+        const statement = db.prepare(sql);
+        const result = statement.all();
+        console.log(result);
+        event.sender.send(request.responseChannel, { result: result });
+      } catch (error) {
+        console.error('Error while handling database event: ', error.message);
+      }
     }
+
+    db.close();
   }
 }
