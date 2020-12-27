d22245ef6bf8abac9cadd18e5435b6153ff24060
Toni Ojala <toni.ojala05@gmail.com>
2020-12-27 11:15:43 +0200

    Add handling of GET_MANY and GET_SINGLE

--- frontend/src/electron/IPC/DatabaseChannel.ts
+++ frontend/src/electron/IPC/DatabaseChannel.ts
@@ -1,7 +1,7 @@
 import { IpcChannel } from './IpcChannel';
 import { IpcMainEvent } from 'electron';
 import sqliteDB from 'better-sqlite3';
-import { IpcRequest } from '../../shared/IpcRequest';
+import { DBRequestType, IpcRequest } from '../../shared/types';
 
 export class DatabaseChannel implements IpcChannel {
   getName(): string { return 'database'; }
@@ -11,12 +11,27 @@ export class DatabaseChannel implements IpcChannel {
 
     const db = new sqliteDB('C:/Users/Toni/Repos/Helsinki University/FinanceTracker/db.db', { verbose: console.log });
 
-    let sql = '';
     if (request.params) {
+      const requestType: DBRequestType = request.params[0] as DBRequestType;
+      const sql = request.params[1];
+      const statement = db.prepare(sql);
+      let result: unknown[] = [];
+
+      console.log('requestType: ', requestType);
+      console.log('sql: ', sql);
+
       try {
-        sql = request.params[0];
-        const statement = db.prepare(sql);
-        const result = statement.all();
+        switch (requestType) {
+          case DBRequestType.GET_MANY:
+            result = statement.all();
+            break;
+          case DBRequestType.GET_SINGLE: {
+            const id = Number(request.params[2]);
+            result = statement.get(id);
+            break;
+          }
+        }
+        console.log('result: ', result);
         event.sender.send(request.responseChannel, { result: result });
       } catch (error) {
         console.error('Error while handling database event: ', error.message);
