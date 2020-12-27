3379bc0e30a125e15fda076aa63d67ac982680b9
Toni Ojala <toni.ojala05@gmail.com>
2020-12-27 21:15:01 +0200

    Add multiple types/enums

--- frontend/src/shared/types.ts
+++ frontend/src/shared/types.ts
@@ -1,16 +1,3 @@
-export interface IpcRequest {
-  responseChannel?: string;
-  params?: unknown[];
-}
-
-export enum DBRequestType {
-  GET_SINGLE = 'getSingle',
-  GET_MANY = 'getMany',
-  POST = 'post',
-  UPDATE = 'update',
-  DELETE = 'delete'
-}
-
 export enum CategoryType {
   Expense = 'expense',
   Income = 'income'
@@ -40,5 +27,34 @@ export interface Budget {
 export type NewCategory = Omit<Category, 'id'>
 export type NewTransaction = Omit<Transaction, 'id'>
 export type NewBudget = Omit<Budget, 'id'>
-
 export type NewModel = NewCategory | NewTransaction | NewBudget
+
+export enum DBTable {
+  CATEGORIES = 'categories',
+  TRANSACTIONS = 'transactions',
+  BUDGETS = 'budgets'
+}
+
+export enum DBRequestType {
+  GET_SINGLE = 'getSingle',
+  GET_MANY = 'getMany',
+  POST = 'post',
+  UPDATE = 'update',
+  DELETE = 'delete'
+}
+
+export interface DBRequestParams {
+  table: DBTable;
+  requestType: DBRequestType;
+  data?: { [key: string]: unknown }
+  query?: { [key: string]: unknown }
+}
+
+export interface IpcRequest {
+  responseChannel?: string;
+  params?: DBRequestParams;
+}
+
+export interface KeyValuePair {
+  [key: string]: unknown;
+}
