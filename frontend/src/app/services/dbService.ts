f4ab144072e87f99ac6a5c77189308a3c8b6d8b2
Toni Ojala <toni.ojala05@gmail.com>
2020-12-27 12:22:15 +0200

    Add post method

--- frontend/src/app/services/dbService.ts
+++ frontend/src/app/services/dbService.ts
@@ -3,17 +3,39 @@ import { send } from "./ipcService";
 
 export async function getMany<T>(table: string): Promise<T> {
   const sql = `SELECT * FROM ${table}`;
-  const result = await send<T>('database', [DBRequestType.GET_MANY, sql]);
+  const result = send<T>('database', [DBRequestType.GET_MANY, sql]);
   return result;
 }
 
 export async function getSingle<T>(table: string, id: number): Promise<T> {
   const sql = `SELECT * FROM ${table} WHERE id = ?`;
-  const result = await send<T>('database', [DBRequestType.GET_SINGLE, sql, id.toString()])
+  const result = send<T>('database', [DBRequestType.GET_SINGLE, sql, id.toString()])
   return result;
 }
 
-// export async function post<T>(url: string, item: T): Promise<T> {
-//   const { data } = await axios.post(url, item);
-//   return data;
-// }
+export async function post<T>(table: string, keys: string[], item: T): Promise<T> {
+  const { keyString, paramsString } = getSqlStrings(keys);
+  
+  const sql = `INSERT INTO ${table} ${keyString} VALUES ${paramsString}`;
+  console.log('sql: ', sql);
+  const result = send<T>('database', [DBRequestType.POST, sql, item]);
+  return result;
+}
+
+function getSqlStrings(keys: string[]) {
+  let keyString = '(';
+  let paramsString = '(';
+
+  keys.forEach(key => {
+    keyString += `${key}, `;
+    paramsString += '?, ';
+  })
+
+  keyString = keyString.substring(0, keyString.length - 2);
+  paramsString = paramsString.substring(0, paramsString.length - 2);
+
+  keyString += ')';
+  paramsString += ')';
+
+  return { keyString, paramsString };
+}
