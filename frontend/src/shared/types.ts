a6594811612c5215a35af8a831cdaab97112c65c
Toni Ojala <toni.ojala05@gmail.com>
2020-12-27 12:02:28 +0200

    Add types fro models

--- frontend/src/shared/types.ts
+++ frontend/src/shared/types.ts
@@ -1,6 +1,6 @@
 export interface IpcRequest {
   responseChannel?: string;
-  params?: string[];
+  params?: unknown[];
 }
 
 export enum DBRequestType {
@@ -10,3 +10,35 @@ export enum DBRequestType {
   UPDATE = 'update',
   DELETE = 'delete'
 }
+
+export enum CategoryType {
+  Expense = 'expense',
+  Income = 'income'
+}
+
+export interface Category {
+  id: string;
+  name: string;
+  type: CategoryType;
+  balance: number;
+}
+
+export interface Transaction {
+  id: string;
+  amount: number;
+  date: string;
+  category: string;
+}
+
+export interface Budget {
+  id: string;
+  amount: number;
+  category: string;
+  startDate: Date;
+}
+
+export type NewCategory = Omit<Category, 'id'>
+export type NewTransaction = Omit<Transaction, 'id'>
+export type NewBudget = Omit<Budget, 'id'>
+
+export type NewModel = NewCategory | NewTransaction | NewBudget
