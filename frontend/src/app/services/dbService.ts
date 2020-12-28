eec6816c61df7d6ff15d02e8a611d60508549735
Toni Ojala <toni.ojala05@gmail.com>
2020-12-28 20:38:35 +0200

    Add query parameter to getMany

--- frontend/src/app/services/dbService.ts
+++ frontend/src/app/services/dbService.ts
@@ -1,19 +1,25 @@
-import { DBTable } from "../../shared/types";
+import { DBTable, KeyValuePair } from "../../shared/types";
 import { send } from "./ipcService";
 
-export async function getMany<T>(table: DBTable): Promise<T> {
-  const requestParams = { table, requestType: 'getMany' };
+export function getMany<T>(table: DBTable, query?: KeyValuePair): Promise<T> {
+  const requestParams = { table, requestType: 'getMany', query };
   const result = send<T>('database', { params: requestParams });
   return result;
 }
 
-export async function getSingle<T>(table: DBTable, id: number): Promise<T> {
+export function getSingle<T>(table: DBTable, id: number): Promise<T> {
   const requestParams = { table, requestType: 'getSingle', query: { id } };
   const result = send<T>('database', { params: requestParams });
   return result;
 }
 
-export async function post<T>(table: DBTable, item: T): Promise<T> {
+export function getCustom<T>(table: DBTable, requestType: string): Promise<T> {
+  const requestParams = { table, requestType };
+  const result = send<T>('database', { params: requestParams });
+  return result
+}
+
+export function post<T>(table: DBTable, item: T): Promise<T> {
   const requestParams = { table, requestType: 'post', data: { item } };
   const result = send<T>('database', { params: requestParams });
   return result;
