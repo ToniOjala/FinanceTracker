c6d8f8de99ff624225bc2d048304b7e49ea241b1
Toni Ojala <toni.ojala05@gmail.com>
2020-12-29 20:50:02 +0200

    Add handling for budget requests

---
+++ frontend/src/electron/IPC/handlers/budgets.ts
@@ -0,0 +1,40 @@
+import { Database } from "better-sqlite3";
+import { Budget, KeyValuePair } from "../../../shared/types";
+
+export function handleBudgetRequest(db: Database, requestType: string, data: KeyValuePair, query?: KeyValuePair ): Budget | Budget[] | KeyValuePair {
+  switch (requestType) {
+    case 'getLatest': {
+      const year = query?.year;
+      const month = query?.month;
+      const latestBudgetPerCategory: KeyValuePair = {};
+
+      if (year && month) {
+        const date = `${year}-${month}-01`;
+        const categories: string[] = db.prepare('SELECT name FROM categories').all();
+
+        for (const category in categories) {
+          try {
+            const sql = `SELECT amount FROM budgets
+                       WHERE category = '${category}'
+                       AND startDate <= date('${date}')
+                       ORDER BY startDate DESC`;
+            const amount = db.prepare(sql).get();
+            latestBudgetPerCategory[category] = amount
+          } catch (_) {
+            latestBudgetPerCategory[category] = 0;
+          }
+        }
+      }
+
+      return latestBudgetPerCategory;
+    }
+    case 'post': {
+      const sql = 'INSERT INTO budgets (amount, startDate, category) VALUES (?, ?, ?)';
+      const { lastInsertRowid } = db.prepare(sql).run(data?.amount, data?.startDate, data?.category);
+      const budget = db.prepare('SELECT * FROM budgets WHERE id = ?').get(lastInsertRowid);
+      return budget;
+    }
+    default:
+      return [];
+  }
+}
