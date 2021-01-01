78408badbcf9c3fdef0b6950c85ecfa25becd9cb
Toni Ojala <toni.ojala05@gmail.com>
2021-01-01 10:09:13 +0200

    Remove backend folder
    The backend connecting to the MongoDB database is no longer needed and therefore removed

--- backend/utils/filters.ts
+++
@@ -1,6 +0,0 @@
-import { ITransaction } from "../models/transaction";
-
-export const filterByMonth = (transactions: ITransaction[], month: number): ITransaction[] => {
-  const monthlyTransactions = transactions.filter(tran => tran.date.getMonth() === month);
-  return monthlyTransactions;
-};
