7f751078edd3d8c86ebec15779d25a877bee7e6e
Toni Ojala <toni.ojala05@gmail.com>
2021-01-02 14:11:09 +0200

    Fix budget getting/posting with single digit month

--- src/electron/IPC/handlers/budgets.ts
+++ src/electron/IPC/handlers/budgets.ts
@@ -4,12 +4,12 @@ import { Budget, Category, KeyValuePair } from "../../../shared/types";
 export function handleBudgetRequest(db: Database, requestType: string, data?: KeyValuePair, query?: KeyValuePair ): Budget | Budget[] | KeyValuePair {
   switch (requestType) {
     case 'getLatest': {
-      const year = query?.year;
-      const month = query?.month;
+      const year = Number(query?.year);
+      const month = Number(query?.month);
       const latestBudgetPerCategory: KeyValuePair = {};
 
       if (year && month) {
-        const date = `${year}-${month}-01`;
+        const date = (month < 10) ? `${year}-0${month}-01` : `${year}-${month}-01`;
         const categories: Category[] = db.prepare('SELECT * FROM categories').all();
 
         categories.forEach((category: Category) => {
@@ -32,7 +32,10 @@ export function handleBudgetRequest(db: Database, requestType: string, data?: Ke
 
       if (budgets) {
         budgets.forEach((budget: Budget) => {
-          const startDate = `${budget.startDate.getFullYear()}-${budget.startDate.getMonth()}-01`;
+          const year = budget.startDate.getFullYear();
+          const month = budget.startDate.getMonth() + 1;
+          const startDate = (month < 10) ? `${year}-0${month}-01` : `${year}-${month}-01`;
+
           const sql = 'INSERT INTO budgets (amount, startDate, category) VALUES (?, ?, ?)';
           const { lastInsertRowid } = db.prepare(sql).run(budget.amount, startDate, budget.category);
           const savedBudget = db.prepare('SELECT * FROM budgets WHERE id = ?').get(lastInsertRowid);
