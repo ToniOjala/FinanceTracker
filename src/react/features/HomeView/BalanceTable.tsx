6aeb43f51564cf58522e4bce52e861615b9e4560
Toni Ojala <toni.ojala05@gmail.com>
2021-01-07 22:01:12 +0200

    Add BalanceTable component #4

---
+++ src/react/features/HomeView/BalanceTable.tsx
@@ -0,0 +1,47 @@
+import { makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
+import React from 'react'
+import { Category } from '../../../shared/types'
+
+const useStyles = makeStyles({
+  headerCell: {
+    width: '28%',
+  },
+  valueCell: {
+    width: '24%',
+  }
+})
+
+interface Props {
+  className: string,
+  categories: Category[],
+}
+
+const BalanceTable = ({ className, categories }: Props): JSX.Element | null => {
+  const classes = useStyles();  
+
+  return (
+    <TableContainer className={className}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.headerCell}>Category</TableCell>
+            <TableCell className={classes.valueCell}>Balance</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {categories.map(category => (
+            <TableRow
+                key={category.name}
+                hover
+              >
+              <TableCell>{category.name}</TableCell>
+              <TableCell>{category.balance}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default BalanceTable;
