aa4f1cb5dd3eb5536873e14a3eeac0e3f6bb6609
Toni Ojala <toni.ojala05@gmail.com>
2021-01-10 19:32:12 +0200

    Add styling

--- src/react/features/AddTransactionDialog/BalancesAccordion.tsx
+++ src/react/features/AddTransactionDialog/BalancesAccordion.tsx
@@ -1,21 +1,53 @@
-import { Accordion, AccordionDetails, AccordionSummary, TextField, Typography } from '@material-ui/core'
+import { Accordion, AccordionDetails, AccordionSummary, makeStyles, TextField, Typography } from '@material-ui/core'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import React from 'react'
 import { Category } from '../../../shared/types'
 
 interface Props {
-  categories: Category[]
+  categories: Category[];
 }
 
-const BalancesAccordion = ({ categories }: Props) => {
+const useStyles = makeStyles({
+  root: {
+    marginTop: '20px',
+    border: 'none',
+    boxShadow: 'none',
+    elevation: '0'
+  },
+  summary: {
+    padding: 0,
+  },
+  list: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  row: {
+    marginBottom: '6px',
+  }
+})
+
+const BalancesAccordion = ({ categories }: Props): JSX.Element | null => {
+  const classes = useStyles();
+  
+  if (!categories) return null;
+
   return (
-    <Accordion>
-      <AccordionSummary>
+    <Accordion className={classes.root}>
+      <AccordionSummary
+        className={classes.summary}
+        expandIcon={<ExpandMoreIcon />}
+      >
         <Typography>Balances</Typography>
       </AccordionSummary>
-      <AccordionDetails>
-        {categories.map((category) => {
-          <TextField id={category.name} label={category.name} />
-        })}
+      <AccordionDetails className={classes.list}>
+        {categories.map(category =>
+          <TextField
+            key={category.name}
+            className={classes.row}
+            id={category.name}
+            label={category.name}
+          />
+        )}
       </AccordionDetails>
     </Accordion>
   )
