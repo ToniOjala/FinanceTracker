7bd122d33fbfca7649fa228f6e11af52b59725e5
Toni Ojala <toni.ojala05@gmail.com>
2021-01-12 19:00:59 +0200

    Add roundToDecimalsAsNumber util method #8

--- src/react/utils/round.ts
+++ src/react/utils/round.ts
@@ -2,3 +2,8 @@ export const roundToDecimals = (value: number, decimals: number): string => {
   if (!value) value = 0;
   return value.toFixed(decimals);
 }
+
+export const roundToDecimalsAsNumber = (value: number, decimals: number): number => {
+  if (!value) value = 0;
+  return Number(value.toFixed(decimals));
+}
