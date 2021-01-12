1f0e9dbd4071fa07768b53369aea6cda15595179
Toni Ojala <toni.ojala05@gmail.com>
2021-01-12 19:15:15 +0200

    Add remaining tests for BalanceList #8

--- src/react/features/AddTransactionDialog/BalancesList.spec.tsx
+++ src/react/features/AddTransactionDialog/BalancesList.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { fireEvent, render, RenderResult } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import BalancesList from './BalancesList';
 import { Category, CategoryType } from '../../../shared/types';
@@ -7,8 +7,8 @@ import { Category, CategoryType } from '../../../shared/types';
 describe('<BalancesList />', () => {
   let categories: Category[];
 
-  const renderWithProps = (categories: Category[], amount: number): RenderResult => {
-    return render(<BalancesList categories={categories} amount={amount} />);
+  const renderWithProps = (categories: Category[], amount: number, component?: RenderResult): RenderResult => {
+    return render(<BalancesList categories={categories} amount={amount} />, component);
   }
 
   const initialize = (amount: number) => {
@@ -51,5 +51,54 @@ describe('<BalancesList />', () => {
       expect(secondInput).toBeDisabled();
       expect(thirdInput).toBeDisabled();
     })
-  })  
+  })
+
+  describe('Amount is a proper number', () => {
+    it('Inputs are enabled', () => {
+      const { firstInput, secondInput, thirdInput } = initialize(900);
+
+      expect(firstInput).toBeEnabled();
+      expect(secondInput).toBeEnabled();
+      expect(thirdInput).toBeEnabled();
+    })
+
+    it('Inputs have proper values', () => {
+      const { firstInput, secondInput, thirdInput } = initialize(900);
+
+      expect(firstInput).toHaveValue('300');
+      expect(secondInput).toHaveValue('300');
+      expect(thirdInput).toHaveValue('300');
+    })
+
+    it('Inputs have proper values if one is overwritten', () => {
+      const { firstInput, secondInput, thirdInput } = initialize(900);
+
+      fireEvent.change(firstInput, { target: { value: '600' }});
+
+      expect(firstInput).toHaveValue('600');
+      expect(secondInput).toHaveValue('150');
+      expect(thirdInput).toHaveValue('150');
+    })
+
+    it('Decimals are handled properly', () => {
+      const { firstInput, secondInput, thirdInput } = initialize(900);
+
+      fireEvent.change(firstInput, { target: { value: '318.91' }});
+
+      expect(firstInput).toHaveValue('318.91');
+      expect(secondInput).toHaveValue('290.54');
+      expect(thirdInput).toHaveValue('290.55');
+    })
+
+    it('Rerendering with new amount prop is handled properly', () => {
+      const { component, firstInput, secondInput, thirdInput } = initialize(900);
+
+      fireEvent.change(firstInput, { target: { value: '600' }});
+      renderWithProps(categories, 1800, component);
+
+      expect(firstInput).toHaveValue('600');
+      expect(secondInput).toHaveValue('600');
+      expect(thirdInput).toHaveValue('600');
+    })
+  })
 })
