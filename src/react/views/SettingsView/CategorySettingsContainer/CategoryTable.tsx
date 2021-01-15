f6d6d27ca2b39fa38ae9a5c85dfbf7c81ce0317f
Toni Ojala <toni.ojala05@gmail.com>
2021-01-15 07:21:44 +0200

    Add CategoryTable component #12

---
+++ src/react/views/SettingsView/CategorySettingsContainer/CategoryTable.tsx
@@ -0,0 +1,54 @@
+import { makeStyles, Card, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, Button } from '@material-ui/core'
+import React from 'react'
+import { Category } from '../../../../shared/types'
+
+const useStyles = makeStyles({
+  headerCell: {
+    width: '28%',
+  },
+  valueCell: {
+    width: '24%',
+  }
+})
+
+interface Props {
+  className: string,
+  categories: Category[],
+}
+
+const CategoryTable = ({ className, categories }: Props): JSX.Element | null => {
+  const classes = useStyles();
+
+  return (
+    <TableContainer
+      className={className}
+      component={Card}
+    >
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.headerCell}>Category</TableCell>
+            <TableCell className={classes.valueCell}><TableSortLabel>Type</TableSortLabel></TableCell>
+            <TableCell className={classes.valueCell}>Created</TableCell>
+            <TableCell className={classes.valueCell}>Removed</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {categories.map(category => (
+            <TableRow
+                key={category.name}
+                hover
+              >
+              <TableCell>{category.name}</TableCell>
+              <TableCell>{category.type[0].toUpperCase() + category.type.substr(1)}</TableCell>
+              <TableCell>{category.created}</TableCell>
+              <TableCell>{category.removed || '-'}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default CategoryTable;
