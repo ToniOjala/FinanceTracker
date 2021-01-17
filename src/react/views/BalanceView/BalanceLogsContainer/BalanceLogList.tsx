5b1a023ccc3641145c77114c3f83bc3430e11077
Toni Ojala <toni.ojala05@gmail.com>
2021-01-17 13:36:10 +0200

    Improve appearance of balance log rows #11

--- src/react/views/BalanceView/BalanceLogsContainer/BalanceLogList.tsx
+++ src/react/views/BalanceView/BalanceLogsContainer/BalanceLogList.tsx
@@ -1,7 +1,10 @@
 import { createStyles, Grid, List, ListItem, ListItemIcon, ListItemText, makeStyles, Theme, Typography } from '@material-ui/core'
-import FolderIcon from '@material-ui/icons/Folder'
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import UpdateIcon from '@material-ui/icons/Update';
 import React from 'react'
 import { BalanceLog } from '../../../../shared/types'
+import { formatDate } from '../../../utils/format';
 
 interface Props {
   balanceLogs: BalanceLog[];
@@ -26,8 +29,15 @@ const BalanceLogList = ({ balanceLogs }: Props) => {
   const classes = useStyles();
 
   function generateText(balanceLog: BalanceLog) {
-    const text = `${balanceLog.reason}ed ${balanceLog.type} of ${balanceLog.amount} on ${balanceLog.date}`;
-    return text;
+    const date = formatDate(balanceLog.date);
+    switch (balanceLog.reason) {
+      case 'add':
+        return `${date} Added an ${balanceLog.type} of ${balanceLog.amount}`;
+      case 'remove':
+        return `${date}: Removed an ${balanceLog.type} of ${balanceLog.amount}`;
+      case 'update':
+        return `${date}: Updated an ${balanceLog.type} by ${balanceLog.amount}`;
+    }
   }
 
   return (
@@ -40,7 +50,9 @@ const BalanceLogList = ({ balanceLogs }: Props) => {
           {balanceLogs.map(bl => 
             <ListItem key={bl.id}>
               <ListItemIcon>
-                <FolderIcon />
+                {bl.reason === 'add' && <AddIcon />}
+                {bl.reason === 'remove' && <RemoveIcon />}
+                {bl.reason === 'update' && <UpdateIcon />}
               </ListItemIcon>
               <ListItemText
                 primary={generateText(bl)}
