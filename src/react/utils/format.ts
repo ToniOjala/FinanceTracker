de0d7688c6dfec857bef320d97c415f835821c84
Toni Ojala <toni.ojala05@gmail.com>
2021-01-19 19:42:41 +0200

    Remove unnecessary format utils

--- src/react/utils/format.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (date: string): string => {
-  if (!date) return '';
-  const splitDate = date.split('-');
-  const year = splitDate[0];
-  const month = splitDate[1];
-  const day = splitDate[2].substr(0, 2);
-
-  return `${day}.${month}.${year}`;
-}
