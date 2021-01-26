11b6f8ac8850425a520ea921b99fdd979d14d251
Toni Ojala <toni.ojala05@gmail.com>
2021-01-26 20:06:31 +0200

    Add tests for BalanceLogList

---
+++ src/react/views/BalanceView/BalanceLogsContainer/BalanceLogList.spec.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import BalanceLogList from './BalanceLogList'
+import { render, screen } from '../../../../__tests__/utils/react';
+import { expect } from 'chai';
+import { BalanceLog } from '../../../../shared/types';
+import { generate } from '../../../../__tests__/utils/generate';
+import { format } from 'date-fns';
+
+function renderWithProps(balanceLogs: BalanceLog[]) {
+  return render(<BalanceLogList className="test" balanceLogs={balanceLogs} />);
+}
+
+describe('<BalanceLogList />', () => {
+  const sampleLogs = generate.balanceLogsWithIds(10);
+
+  it('Indicates balance log inavailability when given no logs', () => {
+    renderWithProps([] as BalanceLog[])
+    const element = screen.getByText('No logs available');
+    expect(element).to.exist;
+  })
+
+  it('shows logs when given a list of balance logs', () => {
+    renderWithProps(sampleLogs);
+    for (const log of sampleLogs) {
+      const text = `${format(new Date(log.date), 'dd.MM.yy')} ${log.amount}`;
+      const element = screen.getByText(text);
+      expect(element).to.exist;
+    }
+  })
+})
