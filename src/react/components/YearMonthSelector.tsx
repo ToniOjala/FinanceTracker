0f26052807c4f6b46b80eb8ef278631d12991b6b
Toni Ojala <toni.ojala05@gmail.com>
2021-03-06 17:02:05 +0200

    Resolve #29

--- src/react/components/YearMonthSelector.tsx
+++ src/react/components/YearMonthSelector.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { ParsableDate } from '@material-ui/pickers/constants/prop-types';
 import DateFnsUtils from '@date-io/date-fns';
-import { makeStyles } from '@material-ui/core';
+import { IconButton, makeStyles } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { setSelectedDate } from '../slices/dateSelection';
-import { format } from 'date-fns';
+import { add, format, sub } from 'date-fns';
+import CustomIcon from './CustomIcon';
 
 const useStyles = makeStyles({
   root: {
-    padding: 0,
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '25px',
     marginTop: '4px',
-    marginRight: '25px'
+  },
+  picker: {
+    padding: 0,
   },
   input: {
     padding: '8px 0 8px 8px',
@@ -37,38 +42,66 @@ const YearMonthSelector = ({ selectedDate, dateSelectionStatus }: Props): JSX.El
 
   if (dateSelectionStatus === 'hidden') return null;
 
+  function incrementMonth() {
+    const newDate = format(add(new Date(selectedDate), { months: 1 }), 'yyyy-MM-dd');
+    dispatch(setSelectedDate(newDate));
+  }
+
+  function decrementMonth() {
+    const newDate = format(sub(new Date(selectedDate), { months: 1 }), 'yyyy-MM-dd');
+    dispatch(setSelectedDate(newDate));
+  }
+
+  function incrementYear() {
+    const newDate = format(add(new Date(selectedDate), { years: 1 }), 'yyyy-MM-dd');
+    dispatch(setSelectedDate(newDate));
+  }
+
+  function decrementYear() {
+    const newDate = format(sub(new Date(selectedDate), { years: 1 }), 'yyyy-MM-dd');
+    dispatch(setSelectedDate(newDate));
+  }
+
   return (
     <>
       {dateSelectionStatus === 'month' && 
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <DatePicker
-            className={classes.root}
-            variant="inline"
-            inputVariant="outlined"
-            inputProps={{ className: classes.input }}
-            views={["month", "year"]}
-            format="MMMM yyyy"
-            value={selectedDate}
-            onChange={handleChange}
-            autoOk
-          />
-        </MuiPickersUtilsProvider>
+        <div className={classes.root}>
+          <IconButton size="small" onClick={decrementMonth}><CustomIcon icon="arrowLeft" /></IconButton>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DatePicker
+              className={classes.picker}
+              variant="inline"
+              inputVariant="outlined"
+              inputProps={{ className: classes.input }}
+              views={["month", "year"]}
+              format="MMMM yyyy"
+              value={selectedDate}
+              onChange={handleChange}
+              autoOk
+            />
+          </MuiPickersUtilsProvider>
+          <IconButton size="small" onClick={incrementMonth}><CustomIcon icon="arrowRight" /></IconButton>
+        </div>
       }
 
       {dateSelectionStatus === 'year' &&
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <DatePicker
-            className={classes.root}
-            variant="inline"
-            inputVariant="outlined"
-            inputProps={{ className: classes.input }}
-            views={["year"]}
-            format="yyyy"
-            value={selectedDate}
-            onChange={handleChange}
-            autoOk
-          />
-        </MuiPickersUtilsProvider>
+        <div className={classes.root}>
+          <IconButton size="small" onClick={decrementYear}><CustomIcon icon="arrowLeft" /></IconButton>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DatePicker
+              className={classes.picker}
+              variant="inline"
+              inputVariant="outlined"
+              inputProps={{ className: classes.input }}
+              views={["year"]}
+              format="yyyy"
+              value={selectedDate}
+              onChange={handleChange}
+              autoOk
+            />
+          </MuiPickersUtilsProvider>
+        <IconButton size="small" onClick={incrementYear}><CustomIcon icon="arrowRight" /></IconButton>
+        </div>
       }
     </>
   )
