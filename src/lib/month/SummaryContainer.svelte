902f70b64d747aa9ac160125bf1acb2c0b1a0f6f
Toni Ojala <toni.ojala05@gmail.com>
2022-07-18 13:02:09 +0300

    Add icons to summaryContainer

--- src/lib/month/SummaryContainer.svelte
+++ src/lib/month/SummaryContainer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Doughnut from "svelte-chartjs/src/Doughnut.svelte";
   import Card from "$lib/components/Card.svelte";
+  import SvgIcon from "$lib/components/SvgIcon.svelte";
   import type { Transaction } from "src/types";
   import { roundToDecimals, roundToDecimalsAsNumber } from "../../utils/round";
 
@@ -61,20 +62,29 @@
 <div class="summary-container">
   <Card margin="0 0 1rem 0">
     <div slot="content" class="summary-content">
-      <h3>Total Income</h3>
-      <span>{roundToDecimals(income, 2)}</span>
+      <SvgIcon icon="arrowUp2" color="#66B088" />
+      <div>
+        <h3>Total Income</h3>
+        <span>{roundToDecimals(income, 2)}</span>
+      </div>
     </div>
   </Card>
   <Card margin="0 0 1rem 0">
     <div slot="content" class="summary-content">
-      <h3>Total Expense</h3>
-      <span>{roundToDecimals(expense, 2)}</span>
+      <SvgIcon icon="arrowDown2" color="#FF5D73" />
+      <div>
+        <h3>Total Expense</h3>
+        <span>{roundToDecimals(expense, 2)}</span>
+      </div>
     </div>
   </Card>
   <Card margin="0 0 1rem 0">
     <div slot="content" class="summary-content">
-      <h3>Balance</h3>
-      <span>{roundToDecimals(income - expense, 2)}</span>
+      <SvgIcon icon="balances" color="#8E90B8" />
+      <div>
+        <h3>Balance</h3>
+        <span>{roundToDecimals(income - expense, 2)}</span>
+      </div>
     </div>
   </Card>
   {#if income > 0}
@@ -101,11 +111,18 @@
     flex-direction: column; 
   }
   .summary-content {
-    padding: 24px 0;
+    padding: 16px 24px;
+    display: flex;
+    align-items: center;
+    justify-content: start;
+  }
+  .summary-content div {
+    margin-left: 2rem;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    margin: auto;
   }
   h3 {
     font-size: 1.3rem;
